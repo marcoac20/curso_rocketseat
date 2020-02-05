35fdc0eb61e040cc99020faefe59b87e3aaab10b
marcoac20 <marcoac@outlook.com>
2020-02-04 23:50:37 -0300

    update

--- Launchbase/Fase2/modulo02/Desafio-02-1-2-3/scripts.js
+++ Launchbase/Fase2/modulo02/Desafio-02-1-2-3/scripts.js
@@ -1,21 +1,22 @@
 const modalOverlay = document.querySelector('.modal-overlay');
 const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
 
 for (let card of cards){
-    card.addEventListener("click", function(){
-        const cursoId = card.getAttribute("id")
-        modalOverlay.classList.add('active')
+    card.addEventListener("click", function(){ //registra um evento
+        const cursoId = card.getAttribute("id") //retorna o valor de um atributo da DOM
+        modalOverlay.classList.add('active') //add uma classe
         modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${cursoId}`;
     })
 }
 
+document.querySelector('.maximize-modal').addEventListener("click", function(){
+    modal.classList.add("maximize")
+})
+
 document.querySelector('.close-modal').addEventListener("click", function(){
     modalOverlay.classList.remove("active")
     modalOverlay.querySelector("iframe").src = "";
-
+    modal.classList.remove("maximize")
 })
 
-document.querySelector('.maximize-modal').addEventListener("click", function(){
-    modalOverlay.fullscreenEnabled.querySelector("iframe").src = `https://rocketseat.com.br/${cursoId}`;
-
-})
