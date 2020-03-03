be8dec67014a5ae72b37aa652e37599503c04e32
marcoac20 <marcoac@outlook.com>
2020-03-02 22:22:47 -0300

    update

---
+++ Launchbase/Fase3/desafios_mod01/data.js
@@ -0,0 +1,49 @@
+module.exports = [
+    {
+        id:"L0zuZYh800Q",
+        title: "Podcast FalaDev #12 - Boas práticas no front-end, técnicas e estratégias",
+        duretion:"37 min",
+        price:"Free",
+        featured: true,
+    },
+
+    {
+        id:"L0zuZYh800Q",
+        title: "Podcast FalaDev #12 - Boas práticas no front-end, técnicas e estratégias",
+        duretion:"37 min",
+        price:"Free",
+        featured: false,
+    },
+
+    {
+        id:"L0zuZYh800Q",
+        title: "Podcast FalaDev #12 - Boas práticas no front-end, técnicas e estratégias",
+        duretion:"37 min",
+        price:"Free",
+        featured: true
+    },
+
+    {
+        id:"L0zuZYh800Q",
+        title: "Podcast FalaDev #12 - Boas práticas no front-end, técnicas e estratégias",
+        duretion:"37 min",
+        price:"Free",
+        featured: false,
+    },
+
+    {
+        id:"L0zuZYh800Q",
+        title: "Podcast FalaDev #12 - Boas práticas no front-end, técnicas e estratégias",
+        duretion:"37 min",
+        price:"Free",
+        featured: false,
+    },
+
+    {
+        id:"L0zuZYh800Q",
+        title: "Podcast FalaDev #12 - Boas práticas no front-end, técnicas e estratégias",
+        duretion:"37 min",
+        price:"Free",
+        featured: true,
+    }
+]
