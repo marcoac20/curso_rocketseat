029620d7ba2f10431b9c94c35c24ac5e6050c6a3
marcoac20 <marcoac@outlook.com>
2020-03-24 22:33:01 -0300

    update

--- Launchbase/Fase3/modulo01/routes.js
+++ Launchbase/Fase3/modulo01/routes.js
@@ -9,6 +9,15 @@ routes.get('/instructors', function(req, res){
     return res.render("instructors/index")
 })
 
+routes.get('/instructors/create', function(req, res){
+    return res.render("instructors/create")
+})
+
+routes.post('/instructors', function(req, res){
+
+    return res.send(req.body) /* req.body = mantém parâmetros enviados do cliente como parte de uma solicitação POST. */
+})
+
 routes.get('/members', function(req, res){
     return res.render("members")
 })
