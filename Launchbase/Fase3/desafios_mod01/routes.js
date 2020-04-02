2191a8d7e6ac05bf8fd3ef4ae098046bd8a49648
marcoac20 <marcoac@outlook.com>
2020-04-01 21:28:11 -0300

    update

--- Launchbase/Fase3/desafios_mod01/routes.js
+++ Launchbase/Fase3/desafios_mod01/routes.js
@@ -4,21 +4,19 @@ const teachers = require('./teachers')
 
 const videos = require("./data")
 
-
-
 routes.get("/", function(req, res){
-    const about = {
-        avatarUrl: "https://avatars1.githubusercontent.com/u/57117820?s=400&v=4",
-        name: "Marco Antunes",
-        role:"Curso Launchbase - Rocketseat",
-        description: 'Dominando a programação do zero com o bootcamp Launchbase da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
-        link: [
-            {name:"Github", url: "https://github.com/marcoac20"},
-            {name:"Instagram", url: "https://www.instagram.com/_marco.ac/"},
-            {name:"Linkdin", url: "https://www.linkedin.com/in/marcoac20/"},
-        ]
-    }
-    return res.render("about", {about})
+  const about = {
+    avatarUrl: "https://avatars1.githubusercontent.com/u/57117820?s=400&v=4",
+    name: "Marco Antunes",
+    role:"Curso Launchbase - Rocketseat",
+    description: 'Dominando a programação do zero com o bootcamp Launchbase da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+    link: [
+      {name:"Github", url: "https://github.com/marcoac20"},
+      {name:"Instagram", url: "https://www.instagram.com/_marco.ac/"},
+      {name:"Linkdin", url: "https://www.linkedin.com/in/marcoac20/"},
+    ]
+  }
+  return res.render("about", {about})
 })
 
 routes.get("/portfolio", function(req, res){
--- Launchbase/Fase3/desafios_mod01/teachers.js
+++ Launchbase/Fase3/desafios_mod01/teachers.js
@@ -1,12 +1,13 @@
 const fs = require('fs')
 const data = require('./data.json')
 
+
 exports.post = function(req, res){
   const keys = Object.keys(req.body)
 
   for(key of keys) {
     if (req.body[key] == ""){
-      return res.send("Por favor, preencha todos os campos")
+      return res.send("Por favor, preencha todos os campos!")
     }
   }
 
